3268f11dbb49fa2cda7555d07ee6d7cfcbb9c3e2
NandiniGajula <113129037+NandiniGajula@users.noreply.github.com>
2024-04-14 19:27:50 +0530

    Update Dse.java

--- Dse.java
+++ Dse.java
@@ -1,22 +1,150 @@
-import javax.crypto.*;
-import java.security.*;
-import java.util.*;
-public class Dse{
-    public static void main(String [] args) throws Exception{
-        KeyPair kp=KeyPairGenerator.getInstance("RSA").generateKeyPair();
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter msg:");
-        String msg=sc.next();
-        Signature signer= Signature.getInstance("SHA256withRSA");
-           signer.initSign(kp.getPrivate());
-           signer.update(msg.getBytes());
-          byte[] signa=signer.sign();
-         System.out.println("Digital Signature:"+Base64.getEncoder().encodeToString(signa));
-         Signature veri= Signature.getInstance("SHA256withRSA");
-           veri.initVerify(kp.getPublic());
-           veri.update(msg.getBytes());
-           boolean isVerified =veri.verify(signa);
-         System.out.println("Signature verified:"+isVerified);
-   }
-}        
+import java.io.*;
  
+public class Main {
+     public static String stringEncryption(String text , String key)
+
+    {    String cipherText = "";
+ 
+
+    
+        int cipher[] = new int[key.length()];
+ 
+
+        for (int i = 0; i < key.length(); i++) {
+
+            cipher[i] = text.charAt(i) - 'A'
+
+                        + key.charAt(i)
+
+                        - 'A';
+
+        }
+ 
+
+     
+
+        for (int i = 0; i < key.length(); i++) {
+
+            if (cipher[i] > 25) {
+
+                cipher[i] = cipher[i] - 26;
+
+            }
+
+        }
+ 
+
+      
+        for (int i = 0; i < key.length(); i++) {
+
+            int x = cipher[i] + 'A';
+
+            cipherText += (char)x;
+
+        }
+ 
+
+    
+        return cipherText;
+
+    }
+ 
+
+    
+    public static String stringDecryption(String s,
+
+                                          String key)
+
+    {
+
+    
+
+        String plainText = "";
+ 
+
+        
+
+        int plain[] = new int[key.length()];
+ 
+
+        
+
+        for (int i = 0; i < key.length(); i++) {
+
+            plain[i]
+
+                = s.charAt(i) - 'A'
+
+                  - (key.charAt(i) - 'A');
+
+        }
+ 
+
+
+        for (int i = 0; i < key.length(); i++) {
+
+            if (plain[i] < 0) {
+
+                plain[i] = plain[i] + 26;
+
+            }
+
+        }
+ 
+
+
+        for (int i = 0; i < key.length(); i++) {
+
+            int x = plain[i] + 'A';
+
+            plainText += (char)x;
+
+        }
+ 
+
+    
+
+        return plainText;
+
+    }
+ 
+
+   
+    public static void main(String[] args)
+
+    {
+
+    
+
+        String plainText = "Hello";
+ 
+
+
+        String key = "MONEY";
+ 
+
+
+        String encryptedText = stringEncryption(
+
+            plainText.toUpperCase(), key.toUpperCase());
+ 
+
+
+
+        System.out.println("Cipher Text - "
+
+                           + encryptedText);
+ 
+
+        
+
+        System.out.println(
+
+            "Message - "
+
+            + stringDecryption(encryptedText,
+
+                               key.toUpperCase()));
+
+    }
+}
